9e16f48ceae6f250b761c339cca6700835153891
Manuela Grimaldi Hansel <manuela.grimaldi@estudante.ufjf.br>
2025-07-29 14:56:21 -0300

    add peças

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ eH1.textContent = "Olá mundo!";
 
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
-const disco1 = criaDisco("preto");
-eTabuleiro.append(disco1);
+
+for(let i=0; i<6; i++){
+    const disco1 = criaDisco(i<3?"branco":"preto");
+    eTabuleiro.append(disco1);
+}
+
+function criaDisco(cor){
+    const novoDisco = document.createElement("div");
+    novoDisco.classList.add("disco");
+    novoDisco.dataset.cor = cor;
+    return novoDisco;
+}
 
 function criaTabuleiro(){
     const eTabuleiro = document.createElement("div");
     eTabuleiro.classList.add("tabuleiro");
     return eTabuleiro;
-}
-
-function criaDisco(){
-    const novoDisco = document.createElement("div");
-    novoDisco.classList.add("disco");
-    novoDisco.dataset.cor = cor;
-    return novoDisco;
 }
