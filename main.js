0b2993782e8eb3c1a6aba01a360a2b16e3cbc219
Manuela Grimaldi Hansel <manuela.grimaldi@estudante.ufjf.br>
2025-07-29 15:06:32 -0300

    posicionando os discos

--- main.js
+++ main.js
@@ -4,15 +4,17 @@ eH1.textContent = "Olá mundo!";
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
 
-for(let i=0; i<6; i++){
-    const disco1 = criaDisco(i<3?"branco":"preto");
+for(let i=0; i<7; i++){
+    if(i===3)continue;
+    const disco1 = criaDisco(i<3?"branco":"preto", i);
     eTabuleiro.append(disco1);
 }
 
-function criaDisco(cor){
+function criaDisco(cor, posicao){
     const novoDisco = document.createElement("div");
     novoDisco.classList.add("disco");
     novoDisco.dataset.cor = cor;
+    novoDisco.dataset.posicao = posicao;
     return novoDisco;
 }
 
